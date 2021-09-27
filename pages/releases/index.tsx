25228f5633c84a1a9cd64e0535cdc714ebb4f3cb
Mikhail Andreev <tundraray@outlook.com>
2021-09-27 20:30:51 +0300

    feat: change view of list

--- pages/releases/index.tsx
+++ pages/releases/index.tsx
@@ -1,8 +1,12 @@
 import Grid from "@xcritical/grid";
+import parse from "date-fns/parse";
+
+import format from "date-fns/format";
 import type { GetServerSideProps, NextPage } from "next";
 import sortBy from "lodash.sortby";
 import Head from "next/head";
 import JiraService, {
+  JiraIssueModel,
   JiraIssuesGroupedByVersionComponent,
 } from "../../services/jira";
 
@@ -37,7 +41,19 @@ const Releases: NextPage<{ versions: JiraIssuesGroupedByVersionComponent }> = ({
   const issues = sortBy(
     uniqBy(Object.values(versions).flat(), "key"),
     (i) => i.version?.fullName
-  );
+  ).reduce<Record<string, JiraIssueModel[]>>((acc, item) => {
+    const releaseDate = item.version?.userReleaseDate
+      ? format(
+          parse(item.version?.userReleaseDate, "dd/LLL/yy", new Date()),
+          "EEEE, dd MMMM yyyy"
+        )
+      : "No release date";
+    if (!acc[releaseDate]) {
+      acc[releaseDate] = [];
+    }
+    acc[releaseDate].push(item);
+    return acc;
+  }, {});
 
   const components = Object.keys(versions).join(", ");
   return (
@@ -50,14 +66,19 @@ const Releases: NextPage<{ versions: JiraIssuesGroupedByVersionComponent }> = ({
       <ContentWrapper>
         <GridWrapper>
           <h1>{components}</h1>
-          <GridInner height={issues.length * 37 + 40}>
-            <Grid
-              rowHeight={37}
-              columns={columns}
-              items={issues}
-              shouldFitContainer
-            />
-          </GridInner>
+          {Object.entries(issues).map(([date, issueList]) => (
+            <>
+              <h2>{date}</h2>
+              <GridInner height={issueList.length * 37 + 40}>
+                <Grid
+                  rowHeight={37}
+                  columns={columns}
+                  items={issueList}
+                  shouldFitContainer
+                />
+              </GridInner>
+            </>
+          ))}
         </GridWrapper>
       </ContentWrapper>
     </div>
